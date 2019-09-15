4f75d0faeb9511161410a7f474393eeb48917bf0
Lio <leo036300@gmail.com>
2019-09-15 12:16:13 +0200

    .d.ts sorted

--- gamebuilder.d.ts
+++ gamebuilder.d.ts
@@ -9,52 +9,39 @@ declare global {
 
     type ActorRef = { comment: "actually a string", docHack: "else vscode hints 'string'" };
 
+    // functions
+
     function clone(original, pos?, rot?): ActorRef;
-
     function deltaTime(): number;
-
-    function getActorsWithTag(tag: string): ActorRef[];
+    function detachFromParentPlease(actor: ActorRef): void;
+    function detachFromParentPlease(actor: ActorRef): void;
     function getActorGroupDescription(pattern: string): string;
+    function getActors(): ActorRef[];
     function getActorsInGroup(pattern: string): ActorRef[];
-    //
+    function getActorsWithTag(tag: string): ActorRef[];
     function getBoundsRadiusOuter(actor: ActorRef, includeVertical?: boolean): number;
     function getBoundsRadiusOuter(includeVertical?: boolean): number;
-    //
+    function getCardTargetActor(propName: string, actionMessage: any): ActorRef | null;
+    function getDisplayName(actor: ActorRef): string;
+    function getParent(actor?: ActorRef): ActorRef | null;
     function getPointBehind(dist: number, actor?: ActorRef): THREE.Vector3;
-    function getCardTargetActor(propName: string, actionMessage: any):
-        ActorRef | null;
     function getThrottle(actor: ActorRef): THREE.Vector3;
     function getTimeSinceReset();
     function getVar(name: string, actor: ActorRef): any;
     function getVelocity(actor: ActorRef): THREE.Vector3;
-
     function isClone(actor?): boolean;
-
     function log(msg: string): void;
-    function lookAt(targetActorOrPoint: ActorRef | THREE.Vector3,
-        yawOnly?: boolean, maxRadians?: number): void;
-
+    function lookAt(targetActorOrPoint: ActorRef | THREE.Vector3, yawOnly?: boolean, maxRadians?: number): void;
+    function myself(): ActorRef;
+    function overlapSphere(center: THREE.Vector3, radius: number): ActorRef;
+    function propActor(prop: string): ActorRef;
+    function propActorGroup(prop: string);
     function propEnum(name: string, defaultValue: string, allowedValues: string[], options?);
-
+    function propNumber(prop: string, val: number);
     function send(actor: ActorRef, messageName: string, arg?: any): void;
     function setPos(worldPos: THREE.Vector3): void;
-    function setVar(name: string, value: any): void;
-
-    function turn(radians: number, axis?: THREE.Vector3): void;
-
-    function vec3(x: number, y: number, z: number): THREE.Vector3;
-
-    // new
-
-    function propActorGroup(prop: string);
-    function propNumber(prop: string, val: number);
-    function getDisplayName(actor: ActorRef): string;
-    function getActors(): ActorRef[];
-    function getParent(actor?: ActorRef): ActorRef | null;
-    function myself(): ActorRef;
-    function detachFromParentPlease(actor: ActorRef): void;
     function setTintHex(colorHex: string): void;
-    function propActor(prop: string): ActorRef;
-    function overlapSphere(center: THREE.Vector3, radius: number): ActorRef;
-    function detachFromParentPlease(actor: ActorRef): void;
+    function setVar(name: string, value: any): void;
+    function turn(radians: number, axis?: THREE.Vector3): void;
+    function vec3(x: number, y: number, z: number): THREE.Vector3;
 }
